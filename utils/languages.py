c969be426e0f8f43128287d8c453994c132452af
Akmaljon701 <akmalkhanever@gmail.com>
2024-10-06 16:04:52 +0500

    fix

--- utils/languages.py
+++ utils/languages.py
@@ -3,6 +3,6 @@ from django.utils import translation
 
 def language(lang):
     if lang:
-        translation.activate('uz')
-    else:
         translation.activate('ru')
+    else:
+        translation.activate('uz')
